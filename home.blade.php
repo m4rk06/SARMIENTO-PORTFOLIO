15b54a22c87b6ada1ee329a5b971d9228716bfb1
m4rk06 <m4rksarmiento@gmail.com>
2025-09-27 21:17:10 +0800

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sarmiento Mark Angel portfolio </title>
+</head>
+<body>
+    <h1> this is my first laravel project</h1>
+</body>
+</html>
